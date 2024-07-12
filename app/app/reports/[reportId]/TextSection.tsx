7e8c18f8032b8b2229960eee2e1e1c92fe42c3c4
Azat222 <azat222@gmail.com>
2024-07-12 21:30:36 +0500

    fix: creation of allSearchWords in TextSection

--- app/app/reports/[reportId]/TextSection.tsx
+++ app/app/reports/[reportId]/TextSection.tsx
@@ -11,14 +11,15 @@ interface TextSectionProps {
 const TextSection = forwardRef(function TextSection({ text, occurrences, activeIndex }: TextSectionProps,
                                                     ref: ForwardedRef<HTMLParagraphElement>) {
         console.log("activeIndex", activeIndex);
-        const allSearchWords = occurrences?.reduce(
-            (prev, occ) => prev.concat(occ.foundVariants), [] as string[]) || [];
+
+        const allSearchWords: string[] = occurrences?.flatMap(item => item.foundVariants)|| [];
+
 
         return (
 
             <div className="overflow-auto flex-1 border-2 rounded-md p-3 border-base-300 resize-none leading-7">
                 <p ref={ref} id="report-text" className="w-full mb-3 text-wrap whitespace-pre-line break-words">
-                    <Highlighter searchWords={allSearchWords} highlightStyle={{ background: "red" }}
+                    <Highlighter searchWords={Array.of(...allSearchWords)} highlightStyle={{ background: "red" }}
                                  activeIndex={activeIndex} activeClassName="active"
                                  textToHighlight={text.replace(/\n{2,}/g, '\n').trim()}/>
                 </p>
